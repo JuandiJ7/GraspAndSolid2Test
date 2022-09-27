d2573d56bce3a178dd33976f2c2dd8594b1bdf9b
Emilio Rodríguez <emilio.rodriguez@correo.ucu.edu.uy>
2022-09-27 15:27:27 -0300

    Eliminando variables innecesarias.

--- src/Test/Library.Test/TestLibrary.cs
+++ src/Test/Library.Test/TestLibrary.cs
@@ -1,8 +1,11 @@
 using NUnit.Framework;
 
+
 namespace Library.Test //Lo que necesitamos probar es cada una de las clases que pertenecen al programa
 {
+    using System.Linq;
     using Full_GRASP_And_SOLID.Library;
+    using System.Collections;
 
     public class Tests
     {        
@@ -10,20 +13,16 @@ namespace Library.Test //Lo que necesitamos probar es cada una de las clases que
         public void ProductTest() //Probamos la creacion y obtencion de un producto
         {
             Product Pan = new Product("Pan",20);
-            var resultado1 = Pan.Description;
-            var resultado2 = Pan.UnitCost;
-            Assert.AreEqual(resultado1,"Pan");
-            Assert.AreEqual(resultado2,20);
+            Assert.AreEqual(Pan.Description,"Pan");
+            Assert.AreEqual(Pan.UnitCost,20);
         }
 
         [Test]
         public void EquipmentTest() //Probamos la creacion y obtencion de un equipamiento
         {
             Equipment Cuchillo = new Equipment("Cuchillo",100);
-            var resultado1 = Cuchillo.Description;
-            var resultado2 = Cuchillo.HourlyCost;
-            Assert.AreEqual(resultado1,"Cuchillo");
-            Assert.AreEqual(resultado2,100);
+            Assert.AreEqual(Cuchillo.Description,"Cuchillo");
+            Assert.AreEqual(Cuchillo.HourlyCost,100);
         } 
 
         [Test]
@@ -32,14 +31,10 @@ namespace Library.Test //Lo que necesitamos probar es cada una de las clases que
             Product Pan = new Product("Pan",20);
             Equipment Cuchillo = new Equipment("Cuchillo",100);
             Step paso1 = new Step(Pan,1,Cuchillo,1);
-            var resultado1 = paso1.Input;
-            var resultado2 = paso1.Quantity;
-            var resultado3 = paso1.Equipment;
-            var resultado4 = paso1.Time;
-            Assert.AreEqual(resultado1,Pan);
-            Assert.AreEqual(resultado2,1);
-            Assert.AreEqual(resultado3,Cuchillo);
-            Assert.AreEqual(resultado4,1);
+            Assert.AreEqual(paso1.Input,Pan);
+            Assert.AreEqual(paso1.Quantity,1);
+            Assert.AreEqual(paso1.Equipment,Cuchillo);
+            Assert.AreEqual(paso1.Time,1);
             Assert.AreEqual(paso1.GetStepCost(),120);
         }
 
@@ -58,13 +53,14 @@ namespace Library.Test //Lo que necesitamos probar es cada una de las clases que
             recipe.AddStep(paso1);
             recipe.AddStep(paso2);
 
-            var resultado = recipe.GetTextToPrint();
+            
             string esperado = $"Receta de {recipe.FinalProduct.Description}:\n" + $"{paso1.Quantity} de '{paso1.Input.Description}' " +
                     $"usando '{paso1.Equipment.Description}' durante {paso1.Time}\n" + $"{paso2.Quantity} de '{paso2.Input.Description}' " +
                     $"usando '{paso2.Equipment.Description}' durante {paso2.Time}\n" + $"Costo de producción: {recipe.GetProductionCost()}"; 
 
-            Assert.AreEqual(resultado, esperado);
+            Assert.AreEqual(recipe.GetTextToPrint(), esperado);
 
         }
-    }
+      
+}
 }
